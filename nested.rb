695f6110a5380bb364a5ba77c5eba90b0c45decb
olusola olagoroye <solaolagoroye@gmail.com>
2019-12-06 02:03:12 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -108,10 +108,10 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     #alans_new_info = "GUI"
+     alans_new_info = "GUI"
      
      programmer_hash[:alan_kay][:known_for] = "GUI"
-     return programmer_hash[:known_for]
+     return programmer_hash[:alan_kay]
 end
 
 def adding_to_dennis
