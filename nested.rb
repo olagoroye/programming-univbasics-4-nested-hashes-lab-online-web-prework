b2633961df5be3635a544ca91c24501cfccd5a03
olusola olagoroye <solaolagoroye@gmail.com>
2019-12-06 02:06:12 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -111,7 +111,7 @@ def changing_alan
      #alans_new_info = "GUI"
      
      programmer_hash[:alan_kay][:known_for] = "GUI"
-     return programmer_hash[:alan_kay][:known_for]
+     return programmer_hash
 end
 
 def adding_to_dennis
