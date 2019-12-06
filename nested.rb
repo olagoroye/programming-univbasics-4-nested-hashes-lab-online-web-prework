1c4968de70c4f4d7120eb3b6d05bd2ec476a1eea
olusola olagoroye <solaolagoroye@gmail.com>
2019-12-06 02:09:12 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -130,5 +130,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+programmer_hash[:dennis_ritchie][:languages] = ["C", "Assembly"]
+return programmer_hash[:dennis_ritchie][:languages]
 end
